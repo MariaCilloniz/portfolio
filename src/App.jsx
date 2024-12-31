5f75115c8f96173e6a7b7fb571f51f8ade21fee2
Maria Cilloniz <majocilloniz@gmail.com>
2024-12-30 17:19:19 -0800

    Fix some issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 
 import './App.scss'
 import React, {useState} from 'react';
-import { BrowserRouter, Route, Routes, Navigate} from 'react-router-dom';
 import Aside from '../src/components/Aside/Aside';
 import Hero from './components/Home/Home';
 import About from '../src/components/About/About';
 import Services from '../src/components/Services/Services';
 import Skills from '../src/components/Skills/Skills';
-import Education from '../src/components/Education/Education';
-import Experience from '../src/components/Experience/Experience';
 import Work from '../src/components/Work/Work';
 import Contact from '../src/components/Contact/Contact';
 import emailjs from '@emailjs/browser'
@@ -44,8 +41,6 @@ function App() {
         <Services id="services" />
         <Skills id="skills" />
         <Work id="work" />
-        {/* <Education id="education" />
-        <Experience id="experience" /> */}
         <Contact id="contact" />
       </main>
     </div>
